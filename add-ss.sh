073090c56109641f24beb692e930ceb0d54903b0
Apeachsan91 <30749674+Apeachsan91@users.noreply.github.com>
2021-07-27 14:07:46 +0800

    Update add-ss.sh

--- add-ss.sh
+++ add-ss.sh
@@ -4,7 +4,7 @@ green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
 echo "Checking VPS"
-IZIN=$( curl curl https://raw.githubusercontent.com/Apeachsan91/server/main/ipallow | grep $MYIP )
+IZIN=$( curl https://raw.githubusercontent.com/Apeachsan91/server/main/ipallow | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 echo -e "${green}Permission Accepted...${NC}"
 else
