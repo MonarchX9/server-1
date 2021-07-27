29f554f472346cd52aa9e4cf542a252e32c425b2
Apeachsan91 <30749674+Apeachsan91@users.noreply.github.com>
2021-07-27 18:04:51 +0800

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -1,92 +1,239 @@
 #!/bin/bash
+color1='\e[031;1m'
+color2='\e[32;1m'
+color3='\e[0m'
 clear
 echo -e ""
-echo -e "=============================-Menu-============================="
-echo -e "* menu         : List of Main Commands"
+echo -e "=====================[ ${color1}AUTOSCRIPT KAIZENVPN${color3} ]==================="
 echo -e ""
-echo -e "=========================-SSH & OpenVPN-========================"
-echo -e "* usernew      : Create SSH & OpenVPN Account"
-echo -e "* trial        : Generate SSH & OpenVPN Trial Account"
-echo -e "* renew        : Extending SSH & OpenVPN Account Active Life"
-echo -e "* deluser      : Delete SSH & OpenVPN Account"
-echo -e "* cek          : Check User Login SSH & OpenVPN"
-echo -e "* member       : Daftar Member SSH & OpenVPN"
-echo -e "* delete       : Delete User Expired SSH & OpenVPN"
-echo -e "* autokill     : Set up Autokill SSH"
-echo -e "* ceklim       : Displays Users Who Do Multi Login SSH"
-echo -e "* restart      : Restart Service Dropbear, Squid3, OpenVPN dan SSH"
+echo -e "============================[ ${color2}MENU${color3} ]============================"
+echo -e "  ${color2}menu${color3}         : Senarai Menu"
 echo -e ""
-echo -e "===========================-Wireguard-========================="
-echo -e "* add-wg       : Create Wireguard Account"
-echo -e "* del-wg       : Delete Wireguard Account"
-echo -e "* cek-wg       : Check User Login Wireguard"
-echo -e "* renew-wg     : Extending Wireguard Account Active Life"
-echo -e "* wg show      : Check Wireguard User Interface"
+echo -e "========================[ ${color2}SSH & OPENVPN${color3} ]======================="
+echo -e " [${color1}1${color3}]     : Membuat akaun SSH & OpenVPN"
+echo -e " [${color1}2${color3}]     : Membuat akaun percubaan SSH & OpenVPN"
+echo -e " [${color1}3${color3}]     : Menambah masa aktif akaun SSH & OpenVPN Account"
+echo -e " [${color1}4${color3}]     : Memadam akaun SSH & OpenVPN Account"
+echo -e " [${color1}5${color3}]     : Senarai login akaun SSH & OpenVPN"
+echo -e " [${color1}6${color3}]     : Senarai akaun SSH & OpenVPN"
+echo -e " [${color1}7${color3}]     : Memadam akaun SSH & OpenVPN yang sudah tamat tempoh"
+echo -e " [${color1}8${color3}]     : Setup Autokill SSH"
+echo -e " [${color1}9${color3}]     : Menunjukkan akaun yang melakukan Multi Login"
+echo -e " [${color1}10${color3}]    : Restart servis Dropbear, Squid3, OpenVPN dan SSH"
 echo -e ""
-echo -e "=============================-L2TP-============================"
-echo -e "* add-l2tp     : Creating L2TP Account"
-echo -e "* del-l2tp     : Deleting L2TP Account"
-echo -e "* renew-l2tp   : Extending L2TP Account Active Life"
+echo -e "==========================[ ${color2}WIREGUARD${color3} ]========================="
+echo -e " [${color1}11${color3}]    : Membuat akaun Wireguard"
+echo -e " [${color1}12${color3}]    : Memadam akaun Wireguard"
+echo -e " [${color1}13${color3}]    : Menambah masa aktif akaun Wireguard"
+echo -e " [${color1}14${color3}]    : Senarai login akaun Wireguard"
+echo -e " [${color1}15${color3}]    : Cek Wireguard User Interface"
 echo -e ""
-echo -e "=============================-PPTP-============================"
-echo -e "* add-pptp     : Create Account PPTP"
-echo -e "* del-pptp     : Delete PPTP Account"
-echo -e "* renew-pptp   : Extending PPTP Account Active Life"
-echo -e "* cek-pptp     : Check User Login PPTP"
+echo -e "============================[ ${color2}SSR${color3} ]============================="
+echo -e " [${color1}16${color3}]    : Membuat akaun SSR"
+echo -e " [${color1}17${color3}]    : Memadam akaun SSR"
+echo -e " [${color1}18${color3}]    : Menambah masa aktif akaun SSR"
+echo -e " [${color1}19${color3}]    : Menu SSR yang lain"
 echo -e ""
-echo -e "=============================-SSTP-============================"
-echo -e "* add-sstp     : Create Account SSTP"
-echo -e "* del-sstp     : Delete SSTP Account"
-echo -e "* renew-sstp   : Extending SSTP Account Active Life"
-echo -e "* cek-sstp     : Check User Login SSTP"
+echo -e "=======================[ ${color2}SHADOWSOCKS OBFS${color3} ]====================="
+echo -e " [${color1}20${color3}]    : Membuat akaun Shadowsocks"
+echo -e " [${color1}21${color3}]    : Memadam akaun Shadowsocks"
+echo -e " [${color1}22${color3}]    : Menambah masa aktif akaun Shadowsocks"
+echo -e " [${color1}23${color3}]    : Senarai login akaun Shadowsocks"
 echo -e ""
-echo -e "=============================-SSR-============================="
-echo -e "* add-ssr      : Create SSR Account"
-echo -e "* del-ssr      : Deleting SSR Account"
-echo -e "* renew-ssr    : Extending SSR Account Active Life"
-echo -e "* ssr          : Show Other SSR Menu"
+echo -e "============================[ ${color2}VMESS${color3} ]==========================="
+echo -e " [${color1}24${color3}]    : Membuat akaun Vmess"
+echo -e " [${color1}25${color3}]    : Memadam akaun Vmess"
+echo -e " [${color1}26${color3}]    : Menambah masa aktif akaun Vmess"
+echo -e " [${color1}27${color3}]    : Senarai login akaun Vmess"
+echo -e " [${color1}28${color3}]    : Renew Certificate akaun V2RAY"
 echo -e ""
-echo -e "========================-Shadowsocks OBFS-====================="
-echo -e "* add-ss       : Creating Shadowsocks Account"
-echo -e "* del-ss       : Delete Shadowsocks Account"
-echo -e "* renew-ss     : Extending Shadowsocks Account Active Life"
-echo -e "* cek-ss       : Check User Login Shadowsocks"
+echo -e "============================[ ${color2}VLESS${color3} ]==========================="
+echo -e " [${color1}29${color3}]    : Membuat akaun Vless"
+echo -e " [${color1}30${color3}]    : Memadam akaun Vless"
+echo -e " [${color1}31${color3}]    : Menambah masa aktif akaun Vless"
+echo -e " [${color1}32${color3}]    : Senarai login akaun Vless"
 echo -e ""
-echo -e "=============================-VMESS-==========================="
-echo -e "* add-ws       : Create V2RAY Vmess Websocket Account"
-echo -e "* del-ws       : Deleting V2RAY Vmess Websocket Account"
-echo -e "* renew-ws     : Extending Vmess Account Active Life"
-echo -e "* cek-ws       : Check User Login V2RAY"
-echo -e "* cert2vray    : Renew Certificate V2RAY Account"
+echo -e "============================[ ${color2}TROJAN${color3} ]=========================="
+echo -e " [${color1}33${color3}]    : Membuat akaun Trojan"
+echo -e " [${color1}34${color3}]    : Memadam akaun Trojan"
+echo -e " [${color1}35${color3}]    : Menambah masa aktif akaun Trojan"
+echo -e " [${color1}36${color3}]    : Senarai login akaun Trojan"
 echo -e ""
-echo -e "=============================-VLESS-==========================="
-echo -e "* add-vless    : Create V2RAY Vless Websocket Account"
-echo -e "* del-vless    : Deleting V2RAY Vless Websocket Account"
-echo -e "* renew-vless  : Extending Vless Account Active Life"
-echo -e "* cek-ws       : Check User Login V2RAY"
+echo -e "===========================[ ${color2}SUBDOMAIN${color3} ]========================"
+echo -e " [${color1}37${color3}]    : Menambah ID Cloudflare"
+echo -e " [${color1}38${color3}]    : Membuat domain dan subdomain pointing ke Cloudflare"
 echo -e ""
-echo -e "=============================-Trojan-=========================="
-echo -e "* add-tr       : Create Trojan Account"
-echo -e "* del-tr       : Deleting Trojan Account"
-echo -e "* renew-tr     : Extending Trojan Account Active Life"
-echo -e "* cek-tr       : Check User Login Trojan"
+echo -e "============================[ ${color2}SISTEM${color3} ]=========================="
+echo -e " [${color1}39${color3}]    : Menukar port servis"
+echo -e " [${color1}40${color3}]    : Autobackup Data VPS"
+echo -e " [${color1}41${color3}]    : Backup Data VPS"
+echo -e " [${color1}42${color3}]    : Restore Data VPS"
+echo -e " [${color1}43${color3}]    : Webmin Menu"
+echo -e " [${color1}44${color3}]    : Limit Bandwith Speed Server"
+echo -e " [${color1}45${color3}]    : Jumlah penggunaan Ram VPS"
+echo -e " [${color1}46${color3}]    : Reboot VPS"
+echo -e " [${color1}47${color3}]    : Speedtest VPS"
+echo -e " [${color1}48${color3}]    : Displaying System Information"
+echo -e " [${color1}49${color3}]    : Info Script Auto Install"
+echo -e " [${color1}50${color3}]    : Exit From VPS"
 echo -e ""
-echo -e "=============================-SYSTEM-=========================="
-echo -e "* add-host     : Add Or Change Subdomain Host For VPS"
-echo -e "* change-port  : Change Port Of Some Service"
-echo -e "* autobackup   : Autobackup Data VPS"
-echo -e "* backup       : Backup Data VPS"
-echo -e "* restore      : Restore Data VPS"
-echo -e "* wbmn         : Webmin Menu"
-echo -e "* kernel-updt  : Update To Latest Kernel"
-echo -e "* limit-speed  : Limit Bandwith Speed Server"
-echo -e "* ram          : Check Usage of VPS Ram"
-echo -e "* reboot       : Reboot VPS"
-echo -e "* speedtest    : Speedtest VPS"
-echo -e "* update       : Update To Latest Script Version"
-echo -e "* info         : Displaying System Information"
-echo -e "* about        : Info Script Auto Install"
-echo -e "* exit         : Exit From VPS "
+echo -e "================================================================"
 echo -e ""
-echo -e "==============================================================="
+read -p "Sila pilih bermula dari [ 1 - 60 ] : " menu
 echo -e ""
+case $menu in
+1)
+usernew
+;;
+2)
+trial
+;;
+3)
+renew
+;;
+4)
+deluser
+;;
+5)
+cek
+;;
+6)
+member
+;;
+7)
+delete
+;;
+8)
+autokill
+;;
+9)
+ceklim
+;;
+10)
+restart
+;;
+11)
+add-wg
+;;
+12)
+del-wg
+;;
+13)
+renew-wg
+;;
+14)
+cek-wg
+;;
+15)
+wg show
+;;
+16)
+add-ssr
+;;
+17)
+del-ssr
+;;
+18)
+renew-ssr
+;;
+19)
+ssr
+;;
+20)
+add-ss
+;;
+21)
+del-ss
+;;
+22)
+renew-ss
+;;
+23)
+cek-ss
+;;
+24)
+add-ws
+;;
+25)
+del-ws
+;;
+26)
+renew-ws
+;;
+27)
+cek-ws
+;;
+28)
+certv2ray
+;;
+29)
+add-vless
+;;
+30)
+del-vless
+;;
+31)
+renew-vless
+;;
+32)
+cek-ws
+;;
+33)
+add-tr
+;;
+34)
+del-tr
+;;
+35)
+renew-tr
+;;
+36)
+cek-tr
+;;
+37)
+add-cff
+;;
+38)
+add-cfh
+;;
+39)
+change-port
+;;
+40)
+autobackup
+;;
+41)
+backup
+;;
+42)
+restore
+;;
+43)
+wbmn
+;;
+44)
+limit-speed
+;;
+45)
+ram
+;;
+46)
+reboot
+;;
+47)
+speedtest
+;;
+48)
+info
+;;
+49)
+about
+;;
+50)
+exit
+;;
+*)
+clear
+menu
+;;
+esac
