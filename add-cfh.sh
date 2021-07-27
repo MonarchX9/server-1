f4df1d62bec67528ef5cf0087bae15089b0a7b52
Apeachsan91 <30749674+Apeachsan91@users.noreply.github.com>
2021-07-27 18:51:52 +0800

    Create add-cfh.sh

---
+++ add-cfh.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+MYIP=$(wget -qO- icanhazip.com);
+echo "Checking VPS"
+IZIN=$( curl https://raw.githubusercontent.com/Apeachsan91/server/main/ipallow | grep $MYIP )
+if [ $MYIP = $IZIN ]; then
+echo -e "${green}Permission Accepted...${NC}"
+else
+echo -e "${red}Permission Denied!${NC}";
+echo "Only For Premium Users"
+exit 0
+fi
+clear
+source /root/mail.conf
+cd
+clear
+echo ""
+read -p "Sila masukkan Domain anda :" domain
+read -p "Sila masukkan SubDomain anda :" sub
+domain=$domain
+#DOMAIN=kaizenvps.xyz
+sub=$sub
+echo -e "SUB_DOMAIN=${sub}.${domain}" >> /root/mail.conf
+SUB_DOMAIN=${sub}.${domain}
+CF_ID=${CF_ID}
+CF_KEY=${CF_KEY}
+#CF_ID=hazboyz@gmail.com
+#CF_KEY=4edad025afe10f3c7adeabe95f810c00a1b5f
+set -euo pipefail
+IP=$(wget -qO- icanhazip.com);
+echo "Updating DNS for ${SUB_DOMAIN}..."
+
+ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${domain}&status=active" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" | jq -r .result[0].id)
+
+RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${SUB_DOMAIN}" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" | jq -r .result[0].id)
+
+if [[ "${#RECORD}" -le 10 ]]; then
+     RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}' | jq -r .result.id)
+fi
+
+RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}')
+echo "DONE...!"
+echo "Your new Domain is : ${domain}"
+echo "Your new SubDomain is : ${SUB_DOMAIN}"
+echo $domain > /root/domain
+echo $SUB_DOMAIN > /root/domain
