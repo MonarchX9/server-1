dff49b0e2beec61cc60dcf675533d32c392294c0
Apeachsan91 <30749674+Apeachsan91@users.noreply.github.com>
2021-07-31 19:06:50 +0800

    Update usernew.sh

--- usernew.sh
+++ usernew.sh
@@ -16,12 +16,6 @@ clear
 read -p "Username : " Login
 read -p "Password : " Pass
 read -p "Expired (hari): " masaaktif
-
-IP=$(wget -qO- icanhazip.com);
-ssl="$(cat ~/log-install.txt | grep -w "Stunnel4" | cut -d: -f2)"
-sqd="$(cat ~/log-install.txt | grep -w "Squid" | cut -d: -f2)"
-ovpn="$(netstat -nlpt | grep -i openvpn | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
-ovpn2="$(netstat -nlpu | grep -i openvpn | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
 sleep 1
 echo Ping Host
 echo Cek Hak Akses...
@@ -36,16 +30,22 @@ sleep 0.5
 clear
 useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $Login
 exp="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
+IP=$(wget -qO- icanhazip.com);
+ssl="$(cat ~/log-install.txt | grep -w "Stunnel4" | cut -d: -f2)"
+sqd="$(cat ~/log-install.txt | grep -w "Squid" | cut -d: -f2)"
+ovpn="$(netstat -nlpt | grep -i openvpn | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
+ovpn2="$(netstat -nlpu | grep -i openvpn | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
 echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
 echo -e ""
 echo -e "==============================="
 echo -e "      MAKLUMAT PORT ANDA       "
 echo -e "==============================="
+echo -e ""
 echo -e "Host            : $IP"
 echo -e "OpenSSH         : 22"
 echo -e "Dropbear        : 109, 143"
-echo -e "SSL/TLS         : $ssl"
-echo -e "Port Squid      : $sqd"
+echo -e "SSL/TLS         :$ssl"
+echo -e "Port Squid      :$sqd"
 echo -e "badvpn          : 7100-7300"
 echo -e "OpenVPN TCP     : TCP $ovpn http://$IP:81/client-tcp-$ovpn.ovpn"
 echo -e "OpenVPN UDP     : UDP $ovpn2 http://$IP:81/client-udp-$ovpn2.ovpn"
@@ -54,9 +54,12 @@ echo -e ""
 echo -e "==============================="
 echo -e "      MAKLUMAT AKAUN ANDA      "
 echo -e "==============================="
+echo -e ""
 echo -e "Username       : $Login "
 echo -e "Password       : $Pass"
 echo -e "Expired pada   : $exp"
+echo -e ""
 echo -e "==============================="
 echo -e " TERIMA KASIH KERANA MELANGGAN "
 echo -e "==============================="
+echo -e ""
