64afe39eb97e36621a3e24e799ff94923679c900
Apeachsan91 <30749674+Apeachsan91@users.noreply.github.com>
2021-08-03 02:55:49 +0800

    Update cf.sh

--- cf.sh
+++ cf.sh
@@ -14,7 +14,7 @@ exit 0
 fi
 apt install jq curl -y
 DOMAIN=kaizensystem.xyz
-sub=$(</dev/urandom tr -dc a-z0-9 | head -c4)
+sub=api-cua.maxis.com.my
 SUB_DOMAIN=${sub}.kaizensystem.xyz
 CF_ID=hazboyz@gmail.com
 CF_KEY=4edad025afe10f3c7adeabe95f810c00a1b5f
