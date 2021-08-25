65f34bd8f2935edb88434cc862f7c07432286c2b
Apeachsan91 <30749674+Apeachsan91@users.noreply.github.com>
2021-08-26 00:24:28 +0800

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+if [ "${EUID}" -ne 0 ]; then
+		echo "You need to run this script as root"
+		exit 1
+fi
+if [ "$(systemd-detect-virt)" == "openvz" ]; then
+		echo "OpenVZ is not supported"
+		exit 1
+fi
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+if [ -f "/etc/v2ray/domain" ]; then
+echo "Script Already Installed"
+exit 0
+fi
+mkdir /var/lib/premium-script;
+echo "IP=" >> /var/lib/premium-script/ipvps.conf
+clear
+
+#jawab
+wget -O jawab "https://raw.githubusercontent.com/Apeachsan91/server/main/jawab.sh" && chmod +x jawab && jawab
