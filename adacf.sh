c3e6728c5ce0347d5adb04914c101aeb36d25688
Apeachsan91 <30749674+Apeachsan91@users.noreply.github.com>
2021-08-26 00:37:05 +0800

    Update adacf.sh

--- adacf.sh
+++ adacf.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
-red='\e[1;31m'
-green='\e[0;32m'
-NC='\e[0m'
-MYIP=$(wget -qO- icanhazip.com);
-echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/Apeachsan91/server/main/ipallow | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
-echo -e "${green}Permission Accepted...${NC}"
-else
-echo -e "${red}Permission Denied!${NC}";
-echo "Only For Premium Users"
-exit 0
-fi
-clear
 cd /root
 CF_ID=${CF_ID}
 CF_KEY=${CF_KEY}
@@ -29,3 +15,44 @@ echo "Email          : ${CF_ID}"
 echo "Api Key        : ${CF_KEY}"
 echo -e "==============================="
 echo -e ""
+read -p "Sila masukkan Domain anda :" domain
+read -p "Sila masukkan SubDomain anda :" sub
+domain=$domain
+sub=$sub
+echo -e "SUB_DOMAIN=${sub}.${domain}" >> /root/mail.conf
+SUB_DOMAIN=${sub}.${domain}
+CF_ID=${CF_ID}
+CF_KEY=${CF_KEY}
+set -euo pipefail
+IP=$(wget -qO- icanhazip.com);
+echo "Updating DNS for ${SUB_DOMAIN}..."
+
+ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${domain}&status=active" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" | jq -r .result[0].id)
+
+RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${SUB_DOMAIN}" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" | jq -r .result[0].id)
+
+if [[ "${#RECORD}" -le 10 ]]; then
+     RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}' | jq -r .result.id)
+fi
+
+RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
+     -H "X-Auth-Email: ${CF_ID}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}')
+echo "DONE...!"
+echo "Your new Domain is : ${domain}"
+echo "Your new SubDomain is : ${SUB_DOMAIN}"
+echo $SUB_DOMAIN > /root/domain
+sleep 2
+wget https://raw.githubusercontent.com/Apeachsan91/server/main/afterjawab.sh && chmod +x afterjawab.sh && ./afterjawab.sh
