084de6df58db085f4f3e7e2cbabfd7054417e765
Apeachsan91 <30749674+Apeachsan91@users.noreply.github.com>
2021-08-26 00:32:38 +0800

    Update jawab.sh

--- jawab.sh
+++ jawab.sh
@@ -6,10 +6,10 @@ read -p "Sekiranya ada, sila tekan 1. Sekiranya tidak ada sila taip 2 (1/2) " ja
 echo -e ""
 case $jawab in
 1)
-adacf
+wget https://raw.githubusercontent.com/Apeachsan91/server/main/adacf.sh && chmod +x adacf.sh && ./adacf.sh
 ;;
 2)
-tiadacf
+wget https://raw.githubusercontent.com/Apeachsan91/server/main/tiadacf.sh && chmod +x tiadacf.sh && ./tiadacf.sh
 ;;
 *)
 clear
