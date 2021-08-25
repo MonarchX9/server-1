0fcb17dcc87a3e34c0c0b4fd58bc90f42cfbf825
Apeachsan91 <30749674+Apeachsan91@users.noreply.github.com>
2021-08-26 00:10:21 +0800

    Update add-cff.sh

--- add-cff.sh
+++ add-cff.sh
@@ -18,8 +18,8 @@ CF_ID=${CF_ID}
 CF_KEY=${CF_KEY}
 read -e -p " Sila masukkan email Cloudflare anda:" CF_ID
 read -e -p " Sila masukkan Api Key Cloudflare anda:" CF_KEY
-echo -e "CF_ID=${CF_ID}" >> /root/mail.conf
-echo -e "CF_KEY=${CF_KEY}" >> /root/mail.conf
+echo -e "CF_ID=${CF_ID}" >> /root/mail1.conf
+echo -e "CF_KEY=${CF_KEY}" >> /root/mail2.conf
 cd
 clear
 echo "DONE...!"
