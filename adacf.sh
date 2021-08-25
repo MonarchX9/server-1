8ddd88e3987f89dd2a880e9c9e5ac115f2fca7f1
Apeachsan91 <30749674+Apeachsan91@users.noreply.github.com>
2021-08-26 02:33:54 +0800

    Update adacf.sh

--- adacf.sh
+++ adacf.sh
@@ -2,6 +2,27 @@
 cd /root
 CF_ID=${CF_ID}
 CF_KEY=${CF_KEY}
+echo "
+                             _---__
+                          _-       /--______
+                     __--( /     \ )XXXXXXXXXXX\v.
+                   .-XXX(   O   O  )XXXXXXXXXXXXXXX-
+                  /XXX(       U     )        XXXXXXX
+                /XXXXX(              )--_  XXXXXXXXXXX
+               /XXXXX/ (      O     )   XXXXXX   \XXXXX
+               XXXXX/   /            XXXXXX   \__ \XXXXX
+              XXXXXX__/          XXXXXX         \__---->
+      ---___  XXX__/          XXXXXX      \__         /
+         \-  --__/   ___/\  XXXXXX            /  ___--/=
+          \-\    ___/    XXXXXX              '--- XXXXXX
+            \-\/XXX\ XXXXXX                      /XXXXX
+               \XXXXXXXXX   \                    /XXXXX/
+                \XXXXXX      >                 _/XXXXX/
+                  \XXXXX--__/              __-- XXXX/
+                   -XXXXXXXX---------------  XXXXXX-
+                      \XXXXXXXXXXXXXXXXXXXXXXXXXX/
+                       ''VXXXXXXXXXXXXXXXXXXV''"
+echo -e ""
 echo -e ""
 echo "==================================="
 echo "  ANDA MEMPUNYAI AKAUN CLOUDFLARE  "
