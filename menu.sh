883933e14cb65d473ae73e0c2246bac7696f0f48
Apeachsan91 <30749674+Apeachsan91@users.noreply.github.com>
2021-08-26 02:34:22 +0800

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -3,6 +3,27 @@ color1='\e[031;1m'
 color2='\e[32;1m'
 color3='\e[0m'
 clear
+echo "
+                             _---__
+                          _-       /--______
+                     __--( /     \ )XXXXXXXXXXX\v.
+                   .-XXX(   O   O  )XXXXXXXXXXXXXXX-
+                  /XXX(       U     )        XXXXXXX
+                /XXXXX(              )--_  XXXXXXXXXXX
+               /XXXXX/ (      O     )   XXXXXX   \XXXXX
+               XXXXX/   /            XXXXXX   \__ \XXXXX
+              XXXXXX__/          XXXXXX         \__---->
+      ---___  XXX__/          XXXXXX      \__         /
+         \-  --__/   ___/\  XXXXXX            /  ___--/=
+          \-\    ___/    XXXXXX              '--- XXXXXX
+            \-\/XXX\ XXXXXX                      /XXXXX
+               \XXXXXXXXX   \                    /XXXXX/
+                \XXXXXX      >                 _/XXXXX/
+                  \XXXXX--__/              __-- XXXX/
+                   -XXXXXXXX---------------  XXXXXX-
+                      \XXXXXXXXXXXXXXXXXXXXXXXXXX/
+                       ''VXXXXXXXXXXXXXXXXXXV''"
+echo -e ""
 echo -e ""
 echo -e "=====================[ ${color1}AUTOSCRIPT KAIZENVPN${color3} ]==================="
 echo -e ""
