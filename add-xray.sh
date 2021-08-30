b6ee9d189023f9520b918fdf0b8d02eaa20aca4f
Apeachsan91 <30749674+Apeachsan91@users.noreply.github.com>
2021-08-30 18:00:26 +0800

    Update add-xray.sh

--- add-xray.sh
+++ add-xray.sh
@@ -37,7 +37,7 @@ exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
 sed -i '$ i\echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6' /etc/rc.local
 
 sed -i '/#tcpxtls$/a\### '"Client $user $exp"'\
-{"id": "'""$uuid""'","email": "'""$user""'"},' /etc/xray/config.json
+"id": "'""$uuid""'","email": "'""$user""'"' /etc/xray/config.json
 vlesslink3="vless://${uuid}@${domain}:$xtls?security=xtls&encryption=none&headerType=none&type=tcp&flow=xtls-rprx-direct&sni=YourISPBug#vless_xtls_${user}"
 systemctl restart xray
 clear
