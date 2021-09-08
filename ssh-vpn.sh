a5fa0213a5bd815d98504c9a6c3a86b514b1400a
Apeachsan91 <30749674+Apeachsan91@users.noreply.github.com>
2021-09-09 00:15:41 +0800

    Update ssh-vpn.sh

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -82,14 +82,14 @@ echo "clear" >> .profile
 echo "neofetch" >> .profile
 
 # install webserver
-#apt -y install nginx
-#cd
-#rm /etc/nginx/sites-enabled/default
-#rm /etc/nginx/sites-available/default
-#wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/Apeachsan91/server/main/nginx.conf"
-#mkdir -p /home/vps/public_html
-#wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/Apeachsan91/server/main/vps.conf"
-#/etc/init.d/nginx restart
+apt -y install nginx
+cd
+rm /etc/nginx/sites-enabled/default
+rm /etc/nginx/sites-available/default
+wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/Apeachsan91/server/main/nginx.conf"
+mkdir -p /home/vps/public_html
+wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/Apeachsan91/server/main/vps.conf"
+/etc/init.d/nginx restart
 
 # install badvpn
 cd
