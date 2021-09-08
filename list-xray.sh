fa6c6797a1bc9a4cae0b92d6ae24da4dedda4806
Apeachsan91 <30749674+Apeachsan91@users.noreply.github.com>
2021-09-09 02:50:53 +0800

    Update list-xray.sh

--- list-xray.sh
+++ list-xray.sh
@@ -12,6 +12,7 @@ echo -e "${red}Permission Denied!${NC}";
 echo "Only For Premium Users"
 exit 0
 fi
+expired=$(date -d "${exp}" +"%d %b %Y")
 clear
 	echo -e ""
 	echo -e "==============================="
