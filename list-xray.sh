7a3103aa385842ce1e4244f25b6c7ce453e27c0b
Apeachsan91 <30749674+Apeachsan91@users.noreply.github.com>
2021-09-09 02:06:13 +0800

    Create list-xray.sh

---
+++ list-xray.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+MYIP=$(wget -qO- icanhazip.com);
+echo "Checking VPS"
+IZIN=$( curl https://raw.githubusercontent.com/Apeachsan91/server/main/ipallow | grep $MYIP )
+if [ $MYIP = $IZIN ]; then
+echo -e "${green}Permission Accepted...${NC}"
+else
+echo -e "${red}Permission Denied!${NC}";
+echo "Only For Premium Users"
+exit 0
+fi
+clear
+	echo -e ""
+	echo -e "==============================="
+	echo -e "Username          Exp. Date"
+	echo -e "-------------------------------"
+	while read expired
+	do
+		user=$(echo $expired | awk '{print $1}')
+		exp=$(echo $expired | awk '{print $3}')
+		exp_date=$(date -d"${exp}" "+%d %b %Y")
+		printf "%-17s %2s\n" "$user" "$exp_date"
+	done < /iriszz/xray/xray-clients.txt
+	total=$(wc -l /iriszz/xray/xray-clients.txt | awk '{print $1}')
+	echo -e "-------------------------------"
+	echo -e "Total accounts: $total"
+	echo -e "==============================="
+	echo -e ""
