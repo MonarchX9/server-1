22e5b22f3ec38726c157a2438239906a08b249ff
Apeachsan91 <30749674+Apeachsan91@users.noreply.github.com>
2021-09-11 22:19:16 +0800

    Update list-xray.sh

--- list-xray.sh
+++ list-xray.sh
@@ -22,7 +22,7 @@ clear
 	do
 		user=$(echo $expired | awk '{print $1}')
 		exp=$(echo $expired | awk '{print $3}')
-		exp_date=$(date -d"${exp}" "+%d %b %Y")
+		exp_date=$(date -d "${exp}" "+%d %b %Y")
 		printf "%-17s %2s\n" "$user" "$exp_date"
 	done < /kaizen/xray/xray-clients.txt
 	total=$(wc -l /kaizen/xray/xray-clients.txt | awk '{print $1}')
