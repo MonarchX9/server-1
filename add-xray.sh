24966d0916159657f24b3f5df81cd74ebeb2f35c
Apeachsan91 <30749674+Apeachsan91@users.noreply.github.com>
2021-09-15 12:54:49 +0800

    Update add-xray.sh

--- add-xray.sh
+++ add-xray.sh
@@ -27,18 +27,19 @@ source /var/lib/premium-script/ipvps.conf
 		exit 0
 	fi
 	read -p "Duration (day) : " duration
-
-	uuid=$(uuidgen)
+	
+	uuid=$(cat /proc/sys/kernel/random/uuid)
 	exp=$(date -d +${duration}days +%Y-%m-%d)
 	expired=$(date -d "${exp}" +"%d %b %Y")
 	domain=$(cat /etc/v2ray/domain)
 	email=${user}@${domain}
-	echo -e "${user}\t${exp}" >> /kaizen/xray/xray-clients.txt
+	echo -e "${user}\t${uuid}\t${exp}" >> /kaizen/xray/xray-clients.txt
 	cat /usr/local/etc/xray/config.json | jq '.inbounds[0].settings.clients += [{"id": "'${uuid}'","flow": "xtls-rprx-direct","email": "'${email}'"}]' > /usr/local/etc/xray/config_tmp.json
 	mv -f /usr/local/etc/xray/config_tmp.json /usr/local/etc/xray/config.json
 	cat /usr/local/etc/xray/config.json | jq '.inbounds[1].settings.clients += [{"id": "'${uuid}'","email": "'${email}'"}]' > /usr/local/etc/xray/config_tmp.json
 	mv -f /usr/local/etc/xray/config_tmp.json /usr/local/etc/xray/config.json
 	service xray restart
+	
 	clear
 echo -e "===========-XRAY/VLESS-=========="
 echo -e "Remarks        : ${user}"
