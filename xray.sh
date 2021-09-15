f1f80cfdf65b7bb505e13163af2f1c7b812fe15e
Apeachsan91 <30749674+Apeachsan91@users.noreply.github.com>
2021-09-16 01:37:13 +0800

    Update xray.sh

--- xray.sh
+++ xray.sh
@@ -14,26 +14,23 @@ sed -i "s/xxx/${domain}/g" /etc/nginx/conf.d/${domain}.conf
 sed -i "s/x.x.x.x/${ip}/g" /etc/nginx/conf.d/${domain}.conf
 
 wget -qO web.tar.gz "https://github.com/Apeachsan91/server/raw/main/web.tar.gz"
-
 rm -rf /var/www/html/*
 tar xzf web.tar.gz -C /var/www/html
 rm -f web.tar.gz
 mkdir -p /kaizen/xray
 #curl -L get.acme.sh | bash
-#/root/.acme.sh/acme.sh --set-default-ca --server letsencrypt
+/root/.acme.sh/acme.sh --set-default-ca --server letsencrypt
 sed -i "6s/^/#/" /etc/nginx/conf.d/${domain}.conf
 sed -i "6a\\\troot /var/www/html/;" /etc/nginx/conf.d/${domain}.conf
 systemctl restart nginx
 #/root/.acme.sh/acme.sh --issue -d "${domain}" --webroot "/var/www/html/" -k ec-256 --force
-#/root/.acme.sh/acme.sh --installcert -d "${domain}" --fullchainpath /kaizen/xray/xray.crt --keypath /kaizen/xray/xray.key --reloadcmd "systemctl restart xray" --ecc --force
-
+#/root/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /kaizen/xray/xray.crt --keypath /kaizen/xray/xray.key --reloadcmd "systemctl restart xray" --ecc --force
+/root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
+/root/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /kaizen/xray/xray.crt --keypath /kaizen/xray/xray.key --ecc
 sed -i "7d" /etc/nginx/conf.d/${domain}.conf
 sed -i "6s/#//" /etc/nginx/conf.d/${domain}.conf
-
-#chown -R nobody.nogroup /kaizen/xray/xray.crt
-#chown -R nobody.nogroup /kaizen/xray/xray.key
-#chown -R nobody.nogroup /etc/v2ray/v2ray.crt
-#chown -R nobody.nogroup /etc/v2ray/v2ray.key
+chown -R nobody.nogroup /kaizen/xray/xray.crt
+chown -R nobody.nogroup /kaizen/xray/xray.key
 
 touch /kaizen/xray/xray-clients.txt
 sed -i "s/\tinclude \/etc\/nginx\/sites-enabled\/\*;/\t# include \/etc\/nginx\/sites-enabled\/\*;asd/g" /etc/nginx/nginx.conf
