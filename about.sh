3fca73ee606f8456b89609b9eec68c8ee2f440b2
MonarchX9 <88991526+MonarchX9@users.noreply.github.com>
2021-12-06 13:28:24 +0800

    Update about.sh

--- about.sh
+++ about.sh
@@ -2,12 +2,12 @@
 #!/bin/bash
 clear
 echo -e "=================================================="
-echo -e "#        Premium Auto Script By KaizenVPN        #"
+echo -e "#        Premium Auto Script By MonarchVPN       #"
 echo -e "#------------------------------------------------#"
 echo -e "#        For Debian 9 & Debian 10 64 bit         #"
 echo -e "#    For Ubuntu 18.04 & Ubuntu 20.04 64 bit      #"
 echo -e "#   For VPS with KVM and VMWare virtualization   #"
-echo -e "#             Build Up By KaizenVPN              #"
+echo -e "#             Build Up By MonarchVPN             #"
 echo -e "#------------------------------------------------#"
 echo -e "#     Terima kasih kerana mencuba servis ini     #"
 echo -e "=================================================="
